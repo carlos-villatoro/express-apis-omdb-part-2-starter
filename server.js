521ebe12a28fcef41f74b2198dbc483080c2338d
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-05-24 17:56:58 -0500

    finishes part2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const axios = require('axios')
+const db = require('./models')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -36,6 +37,24 @@ app.get('/details/:id', (req, res) => {
       res.render('detail.ejs', { movie: response.data })
     })
     .catch(console.log)
+})
+
+app.get('/faves', async (req, res)=>{
+  // get all faves from db
+  const allFaves = await db.fave.findAll()
+  // render a faves page
+  res.render('faves.ejs', {allFaves})
+})
+
+app.post('/faves', async (req, res)=>{
+  // console.log(req.body)
+  // create new fave in db
+  await db.fave.create({
+    title: req.body.title,
+    imdbID: req.body.imdbid
+  })
+  // redirect to show all faves
+  res.redirect('/faves')
 })
 
 app.listen(process.env.PORT || 3000);
